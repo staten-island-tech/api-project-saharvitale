9b89de504d222105ac6484b60e23d5660e638d65
saharvitale <saharvictoria143@gmail.com>
2023-02-02 15:17:45 -0500

    api gna cry

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -26,33 +26,77 @@
 // getData(URL);
 import "../styles/style.css";
 
-const meme= document.getElementById("meme");
-const title= document.getElementById("title");
-const getMemeBtn= document.getElementById("get-meme-btn");
+const DOM = {
+    reponse: document.querySelector(".api-reponse"),
+    Input: document.querySelector(".UserInput"),
+    submit: document.querySelector("#form"),
+    list: document.querySelector(".list"),
+  };
 
+const URL = `https://www.themealdb.com/api/json/v1/1/categories.php`;
 
-//api url
-const url = "https://meme-api.com/gimme/";
-//array of subreddits of ur choice
-let subreddits= ["catmemes", "anime", "dankmemes"]
+async function getData(URL) {
+  try {
+    const MealInfo = await fetch(URL);
 
-//get  random meme
-let getMeme = () => {
-// choose random subreddit from subreddits array
-    let randomSubreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
-   //fetch data from the api
-   fetch(url + randomSubreddit)
-   .then((resp) => resp.json())
-   .then((data) => {
-    const memeImg = new Image();
-    //display meme img and title only after img loads
-    memeImg.onload = () => {
-        memeImg.src = data.url;
-        document.getElementById("title").textContent = data.title;
+    if (MealInfo.status <= 199 || MealInfo.status >= 300) {
+      throw new Error(MealInfo);
+    } else {
+      MealList();
+      getInfo();
     }
-    memeImg.src = data.url;
-   });
-};
-//call getMeme() on button click and widnow reload
-getMemeBtn.addEventListener("click", getMeme);
-window.addEventListener("load", getMeme);
+  } catch (error) {
+    console.log(error);
+    console.log("Nope");
+  }
+}
+
+function ClearInput() {
+  document.querySelector(".UserInput").value = "";
+}
+
+function removal(event) {
+  event.target.parentElement.remove();
+}
+
+DOM.submit.addEventListener("submit", async function (abc) {
+  abc.preventDefault();
+  await getInfo();
+  ClearInput();
+  document.querySelectorAll(".remove").forEach((button) => {
+    button.addEventListener("click", removal);
+  });
+});
+
+async function MealList() {
+  const MealInfo = await fetch(URL);
+  const MealData = await MealInfo.json();
+  MealData.categories.forEach((el) => {
+    DOM.list.insertAdjacentHTML("beforeend", `<li>${el.strCategory}</li>`);
+  });
+}
+//displays list of meals available in api
+
+async function getInfo() {
+  const MealInfo = await fetch(URL);
+  const MealData = await MealInfo.json();
+  let UserInputIM = DOM.Input.value;
+  const UserInput = UserInputIM.charAt(0).toUpperCase() + UserInputIM.slice(1);
+
+  MealData.categories
+    .filter((el) => el.strCategory == `${UserInput}`)
+    .map((el) => {
+      DOM.reponse.insertAdjacentHTML(
+        "beforeend",
+        `<div class="suu">
+            <img src="${el.strCategoryThumb}" alt="">
+            <h1>${el.strCategory}</h3>
+            <h3>description: ${el.strCategoryDescription}</h3>
+            <button class="remove">Clear</button>
+          
+          </div>`
+      );
+    });
+}
+
+getData(URL);
